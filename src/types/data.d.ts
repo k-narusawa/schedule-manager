74184cdca2dd17999846a22f8686448f43106905
k-narusawa <swkei320@gmail.com>
2022-09-21 21:25:44 +0900

    add: 型定義を追加

---
+++ src/types/data.d.ts
@@ -0,0 +1,69 @@
+export type calendarEvent = {
+  id: string;
+  status: string;
+  htmlLink: string;
+};
+
+export type calendarApiResponse = {
+  kind: string;
+  etag: string;
+  summary: string;
+  updated: Date;
+  timeZone: string;
+  accessRole: string;
+  defaultReminders: {
+    method: string;
+    minutes: number;
+  };
+  nextPageToken: string;
+  items: Array<calendarApiResponseItem>;
+};
+
+export type calendarApiResponseItem = {
+  kind: string;
+  etag: string;
+  id: string;
+  status: string;
+  htmlLink: string;
+  created: Date;
+  updated: Date;
+  summary: string;
+  description: string;
+  location: string;
+  creator: {
+    email: string;
+    self: boolean;
+  };
+  organizer: {
+    email: string;
+    self: boolean;
+  };
+  start: {
+    dateTime: Date;
+    timeZone: string;
+  };
+  end: {
+    dateTime: Date;
+    timeZone: string;
+  };
+  endTimeUnspecified: boolean;
+  transparency: string;
+  visibility: string;
+  iCalUID: string;
+  sequence: string;
+  attendees: Array<{
+    email: string;
+    self: boolean;
+    responseStatus: boolean;
+  }>;
+  guestsCanInviteOthers: boolean;
+  privateCopy: boolean;
+  reminders: {
+    useDefault: boolean;
+  };
+  source: {
+    url: string;
+    title: string;
+  };
+  eventType: string;
+};
